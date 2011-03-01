4a367942ced6d498d9e7cc55af92cdf9b2328f72
jeffbr13 <jeffbr13@gmail.com>
2011-03-01 20:15:38 +0000

    MultiMU restructured. Needs debugging.

--- MultiMU.py
+++ MultiMU.py
@@ -11,58 +11,57 @@ def ApplyRules (WordList):
     for Word in WordList:
         WordDerivatives = Rules.Transform(Word)
         Derivations.extend(WordDerivatives)
-     return Derivations
+    return Derivations
 
+def DoCycles (EndWord, WordList, MaxCycles, Workers=1):
 
-def CycleThrough (WordList, Workers=1):
-
+    ppservers = ()
+    JobServer = pp.Server(Workers, ppservers=ppservers)
+    print 'Job server started with %s worker processes.' % str(JobServer.get_ncpus())	
     int (Workers)
     
     Words = len(WordList)
     
-    ChunkSize= Words/Processors
+    ChunkSize= Words/Workers
     
     Marker = 0
     Count = 1
     
-    Derivations = []
-    
-    for Process in range(0,Workers):
-        OwnChunk = WordList[Marker:(ChunkSize*Count)]
-        Marker = (ChunkSize*Count)
-        Count += 1
-        Job = JobServer.submit(ApplyRules, (OwnChunk,), (Transform,), ('Rules',))
-        Result = Job()
-        Derivations.extend(Result)
-        # DO STUFF HERE
-    
-#    for Word in WordList:               # For every word in the list,
-#        Derivatives = MU.Transform(Word)   # get it's transformations,
-#        Derivations += Derivatives      # and put them in a big list.
-#    return Derivations                  # Return the list for the next cycle.
-
-def Run (MaxCycles=10, StartWord='MI', EndWord='MU'):
-
-    ppservers = ()
-    NumCPUs = 2
-	JobServer = pp.Server(NumCPUs, ppservers=ppservers)
-	print 'Job server started with %s worker processes.' % str(JobServer.get_ncpus())
-	
-    Derivatives = [StartWord]   # List of words
     Cycles = 0
-
-    while EndWord not in Derivatives:
-
+    
+    while EndWord not in WordList:
         Cycles += 1
-        print Cycles, str(len(Derivatives))
-        if Cycles >= MaxCycles: # Stop if we can't find it in given number of cycles.
-            return 'Could not derive in %s cycles' % MaxCycles
+        print Cycles
+        print str(len(WordList))
+        
+        if Cycles >= MaxCycles:
+            return "Could not derive in %s cycles." % MaxCycles
+            
         else:
-            Derivatives += CycleThrough(Derivatives)
+        
+            Derivations = []
+            for Process in range(0,Workers):
+                OwnChunk = WordList[Marker:(ChunkSize*Count)]
+                Marker = (ChunkSize*Count)
+                Count += 1
+                Job = JobServer.submit(ApplyRules, (OwnChunk,), (Transform,), ('Rules',))
+                Result = Job()
+                Derivations.extend(Result)
+            
+            WordList.extend(Derivations)
+
+    return ("Success!", Cycles, Endword,)
+
+
+def Run (MaxCycles=10, StartWord='MI', EndWord='MIU'):
+
+
+    Derivatives = [StartWord]   # List of words
+
                                             
     return (Cycles, StartWord, EndWord) # Returns a tuple at the end, if found.
 	
 	
 
 if __name__ == '__main__':
-    main()
+    print Run()
