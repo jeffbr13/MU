7266214a505d4cb7dffa367faaa8cd25cfbbc2e4
jeffbr13 <jeffbr13@gmail.com>
2011-03-01 19:49:48 +0000

    'Rules' changed to 'Rules.py' so importable as a module. Advancements in MultiMU.

--- MultiMU.py
+++ MultiMU.py
@@ -1,34 +1,53 @@
 #!/usr/bin/python
 # Copyright (C) 2011 Ben Jeffrey. All Rights Reserved.
-import MU
+import Rules
 import pp
 import sys
 
+Transform = Rules.Transform
 
-def main():
-	"""
-	The main program loop goes here.
-	"""
-	ppservers = ()
-	
-	NumCPUs = 2
-	
-	JobServer = pp.Server(NumCPUs, ppservers=ppservers)
-	
-	print "Job Server started with %s worker processes." % JobServer.get_ncpus()
-	
-def CycleThrough (WordList):
+def ApplyRules (WordList):
     Derivations = []
-    for Word in WordList:               # For every word in the list,
-        Derivatives = Transform(Word)   # get it's transformations,
-        Derivations += Derivatives      # and put them in a big list.
-    return Derivations                  # Return the list for the next cycle.
+    for Word in WordList:
+        WordDerivatives = Rules.Transform(Word)
+        Derivations.extend(WordDerivatives)
+     return Derivations
+
+
+def CycleThrough (WordList, Workers=1):
+
+    int (Workers)
+    
+    Words = len(WordList)
+    
+    ChunkSize= Words/Processors
+    
+    Marker = 0
+    Count = 1
+    
+    Derivations = []
+    
+    for Process in range(0,Workers):
+        OwnChunk = WordList[Marker:(ChunkSize*Count)]
+        Marker = (ChunkSize*Count)
+        Count += 1
+        Job = JobServer.submit(ApplyRules, (OwnChunk,), (Transform,), ('Rules',))
+        Result = Job()
+        Derivations.extend(Result)
+        # DO STUFF HERE
+    
+#    for Word in WordList:               # For every word in the list,
+#        Derivatives = MU.Transform(Word)   # get it's transformations,
+#        Derivations += Derivatives      # and put them in a big list.
+#    return Derivations                  # Return the list for the next cycle.
 
 def Run (MaxCycles=10, StartWord='MI', EndWord='MU'):
 
     ppservers = ()
-    NumCPUs = 
-
+    NumCPUs = 2
+	JobServer = pp.Server(NumCPUs, ppservers=ppservers)
+	print 'Job server started with %s worker processes.' % str(JobServer.get_ncpus())
+	
     Derivatives = [StartWord]   # List of words
     Cycles = 0
 
